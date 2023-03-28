ef6e592b43a905028eba12051e4b8e67f693259a
Jhunel Peñaflorida <jhunel.penaflorida@novare.com.hk>
2023-03-28 23:25:01 +0800

    Added the board.

--- app/src/main/java/ph/stacktrek/novare/ecommercenovare/penaflorida_dellota/snakeladders/MainActivity.kt
+++ app/src/main/java/ph/stacktrek/novare/ecommercenovare/penaflorida_dellota/snakeladders/MainActivity.kt
@@ -1,11 +1,53 @@
 package ph.stacktrek.novare.ecommercenovare.penaflorida_dellota.snakeladders
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var board: Array<IntArray>
+    private var boardSize = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        board = Array(boardSize) { IntArray(boardSize) }
+
+        generateBoard()
+    }
+        private fun generateBoard() {
+        var row = boardSize - 1
+        var cellValue = 1
+        for (i in 0 until boardSize) {
+            for (j in 0 until boardSize) {
+                board[row][if (row % 2 == 0) j else boardSize - j - 1] = cellValue
+                cellValue++
+            }
+            row--
+        }
+
+        val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
+        for (i in 0 until boardSize) {
+            val tableRow = TableRow(this)
+            tableRow.layoutParams = TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.MATCH_PARENT,
+                1.0f
+            )
+            tableLayout.addView(tableRow)
+            for (j in 0 until boardSize) {
+                val textView = TextView(this)
+                textView.text = board[i][j].toString()
+                textView.setBackgroundResource(R.drawable.cell_bg)
+                textView.layoutParams = TableRow.LayoutParams(
+                    TableRow.LayoutParams.MATCH_PARENT,
+                    TableRow.LayoutParams.MATCH_PARENT,
+                    1.0f
+                )
+                tableRow.addView(textView)
+            }
+        }
     }
 }
